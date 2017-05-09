dc150916c416f571bde432493fc7388929b3a694
Pavlo Kryshenyk <kryshenykpavlo@gmail.com>
2017-05-09 09:28:03 +0200

    js task 1,2 first fixes

--- kryshenyk_js_1_2/js/task1.js
+++ kryshenyk_js_1_2/js/task1.js
@@ -1,14 +1,26 @@
 var base = parseInt(prompt('Enter the base: ', ''));
+
+while (isNaN(base)) {
+  base = parseInt(prompt('Value, you\'ve entered, is not a number! Please enter a base as number again.', ''));
+}
+
 var power = parseInt(prompt('Enter the power: ', ''));
-var result = 1;
+
+while (isNaN(power)) {
+  power = parseInt(prompt('Value, you\'ve entered, is not a number! Please enter a power as number again.', ''));
+}
+
+
 
 function calcPow(base, power) {
+  var result = 1;
+
   if (power > 0) {
     for (i = 1; i <= power; i++) {
-    result = result * base;
-  }
-} else if (power < 0) {
-  power = -power;
+      result = result * base;
+    }
+  } else if (power < 0) {
+    power = -power;
     for (i = 1; i <= power; i++) {
       result = result * base;
     }
--- kryshenyk_js_1_2/js/task2.js
+++ kryshenyk_js_1_2/js/task2.js
@@ -1,17 +1,20 @@
 var arrName = [];
-var validationMsg = 'Wrong name!!!';
 
 for (var i = 0; i < 5; i++) {
   arrName.push(prompt('Enter name: try ' + (i + 1), ''));
 }
 
-var nameCheck = prompt('Enter the name!', '');
+function checkName(nameCheck) {
+  var nameCheck = prompt('Enter the name!', '');
+  var validationMsg = 'Wrong name!!!';
 
-for(var i = arrName.length - 1; i >= 0; i--) {
-  if (arrName[i] == nameCheck) {
-    validationMsg = nameCheck + ', you\'ve successfully entered!';
-    break;
+  for (var i = 0; i < 5; i++) {
+    if (arrName[i] === nameCheck) {
+      validationMsg = nameCheck + ', you\'ve successfully entered!';
+      break;
+    }
   }
+  alert(validationMsg);
 }
 
-alert (validationMsg);
+checkName();
