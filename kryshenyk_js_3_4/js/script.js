719cb6b2c5fd4098b1e3ad4bc6b84608c7a2f15c
Pavlo Kryshenyk <kryshenykpavlo@gmail.com>
2017-06-12 00:18:53 +0200

    WIP

---
+++ kryshenyk_js_3_4/js/script.js
@@ -0,0 +1,71 @@
+/*
+Written by Pavlo Kryshenyk 10.5.2017
+*/
+document.onload = (function generateForm() {
+  'use strict';
+
+  var root = document.createElement('div');
+  root.setAttribute("id", "root");
+  document.body.appendChild(root);
+
+  var formContainer = document.createElement('form');
+  formContainer.classList.add("formContainer");
+  root.appendChild(formContainer);
+
+  var pageHeading = document.createElement('h1');
+  pageHeading.classList.add("centred", "main-heading");
+  pageHeading.appendChild(document.createTextNode('Javascript Programming Quiz'));
+  formContainer.appendChild(pageHeading);
+
+  var quiz = {
+    questions: function () {
+
+      var questionsList = document.createElement("div");
+      questionsList.classList.add("ql-container");
+      formContainer.appendChild(questionsList);
+
+      for (var i = 1; i < 4; i++) {
+        var question = document.createElement('ul');
+        questionsList.appendChild(question);
+        var caption = document.createElement('h4');
+        caption.appendChild(document.createTextNode(i +". " + "Question # " + i));
+        question.appendChild(caption);
+
+        for (var j = 1; j <= 3 ; j++) {
+          var answerContainer = document.createElement('li');
+          answerContainer.classList.add("list-unstyled");
+          question.appendChild(answerContainer);
+
+          var answer = document.createElement('label');
+          answer.classList.add("my-label");
+          answer.setAttribute("id", "checkbox");
+          answerContainer.appendChild(answer);
+
+          var input = document.createElement('input');
+          input.setAttribute("type", "checkbox");
+          answer.appendChild(input);
+
+
+          var crm = document.createElement('span');
+          crm.appendChild(document.createTextNode(' Option # ' + j));
+          answer.appendChild(crm);
+        }
+      }
+    },
+
+    button: function () {
+      var button = document.createElement("button");
+      button.classList.add("btn", "btn-default", "centred", "my-button");
+      formContainer.appendChild(button);
+
+      var buttonCaption = document.createElement("p");
+      buttonCaption.appendChild(document.createTextNode('Submit Form'));
+      buttonCaption.classList.add("btn-caption");
+      button.appendChild(buttonCaption);
+    }
+  }
+
+  quiz.questions();
+  quiz.button();
+
+})();
