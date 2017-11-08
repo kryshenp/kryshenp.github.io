fdc5164158298ff80b8d6561dccbfbd38ecf30be
Pavlo Kryshenyk <Pavlo.Kryshenyk@4finance.com>
2017-11-08 14:39:56 +0100

    HW11-12 done

--- kryshenyk_js_11_12/js/second.js
+++ kryshenyk_js_11_12/js/second.js
@@ -1,30 +1,10 @@
 $(function() {
 
-  // var html = $('#test').html();
-  //
-  // var articles = [
-  //   {
-  //     title: 'Hello world',
-  //     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  //   },
-  //   {
-  //     title: 'Hello world 2',
-  //     content: 'od tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  //   },
-  //   {
-  //     title: 'Hello world 3',
-  //     content: 'od tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  //   }
-  // ];
-  //
-  // var content = tmpl(html, {
-  //   data: articles
-  // });
   var html = $('#profile').html();
 
   var points = [
     {
-      point0: 'Студент Карлова университета',
+      point0: "Charles university student",
     },
     {
       title: 'Хочу учить фронтенд, потому что:',
