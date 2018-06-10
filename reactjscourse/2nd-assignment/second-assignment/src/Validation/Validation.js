2671b8bb8105672e733a497bb5b5fa5d44a9045b
Pavlo Kryshenyk <pavlokryshenyk@Pavlos-MacBook-Pro.local>
2018-06-10 21:29:58 +0200

    2assignment 3d task

--- reactjscourse/2nd-assignment/second-assignment/src/Validation/Validation.js
+++ reactjscourse/2nd-assignment/second-assignment/src/Validation/Validation.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 const validation = (props) => {
+  let validationMessage = 'Text long enough';
+
+  if (props.inputTextLength <= 5) {
+    validationMessage = 'Text too short';
+  }
+
   return (
     <div>
-      <p>Text too short!</p>
+      <p>{validationMessage}</p>
     </div>
   );
 };
 
 export default validation;
+
+// this is a 1st way how to output validations
+// { props.inputTextLength < 5 ?
+//     <p>Text too short!</p> :
+//     <p>Text long enough</p>
+// }
