581fa552097ed32517d6754e3fa456bed2c687c4
Pavlo Kryshenyk <pavlokryshenyk@Pavlos-MacBook-Pro.local>
2018-08-08 22:51:38 +0200

    5-68-Adding Pseudo Selector

--- reactjscourse/pavlo-app/5-section/src/App.js
+++ reactjscourse/pavlo-app/5-section/src/App.js
@@ -72,16 +72,9 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      color: 'white'
-    };
 
     let persons = null;
+    let btnClass = '';
 
     if ( this.state.showPersons ) {
       persons = (
@@ -97,8 +90,9 @@ class App extends Component {
           })}
       </div>
       );
-      // if we click on the button its color is red 5.62
-      style.backgroundColor = 'red';
+
+      btnClass = classes.Red;
+
     }
 
     // create an array classes and join them wit an
@@ -121,8 +115,8 @@ class App extends Component {
           <h1>Hi, I'm a React App</h1>
           <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button
-             style={style}
-             onClick={this.togglePersonsHandler}>Toggle persons</button>
+              className={btnClass}
+              onClick={this.togglePersonsHandler}>Toggle persons</button>
           {persons}
         </div>
     );
