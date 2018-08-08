bcc6665621ba294de16d8d521e6a1d81daf2b3e4
Pavlo Kryshenyk <pavlokryshenyk@Pavlos-MacBook-Pro.local>
2018-08-08 20:32:04 +0200

    WIP

--- reactjscourse/pavlo-app/2-lecture-B/src/App.js
+++ reactjscourse/pavlo-app/2-lecture-B/src/App.js
@@ -7,34 +7,63 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Pavlo', age: 30 },
-      { name: 'Veronika', age: 29 },
-      { name: 'X', age: 0 },
+      { id: 'soiadif', name: 'Pavlo', age: 30 },
+      { id: 'sadiljf', name: 'Veronika', age: 29 },
+      { id: 'sdilfud', name: 'X', age: 0 },
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // DON'T DO THIS: this.state.persons[0].name = 'Pavlo';
-    // console.log('Was clicked!');
-    this.setState( {
-      persons: [
-        { name: newName, age: 30 },
-        { name: 'Veronika', age: 29 },
-        { name: 'X', age: 1 },
-      ]
-    } )
+  // switchNameHandler = (newName) => {
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Pavlo';
+  //   // console.log('Was clicked!');
+  //   this.setState( {
+  //     persons: [
+  //       { name: newName, age: 30 },
+  //       { name: 'Veronika', age: 29 },
+  //       { name: 'X', age: 1 },
+  //     ]
+  //   } )
+  // }
+
+  nameChangedHandler = (event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    // const person = Object.assign({},this.state.persons[personIndex]);
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons}  );
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Pavlo', age: 30 },
-        { name: event.target.value, age: 29 },
-        { name: 'X', age: 0 },
-      ]
-    } )
+  deletePersonHandler = (personIndex) => {
+      // get access to all the persons in the state
+      // create a copy of the persons array before manipulating it
+      // by adding the slice method
+      // slice without arguments simply copies the the full array and
+      // and turns the new one which is then stored here
+
+      // const persons = this.state.persons.slice();
+
+      // alternative to this approach would be to use ES6 feature - the spread operator
+      // ... is a spread operator
+      // spreads out the elements in this array into a list of elements which simply
+      // then gets added to this array
+      // so now we have a new array with the objects from the old array but not array itself
+      const persons = [...this.state.persons];
+
+      // removing one elemet we wanted to remove by using the index of the person
+      persons.splice(personIndex, 1);
+      // updating the react  state with my new array
+      this.setState({persons: persons})
   }
 
   togglePersonsHandler = () => {
@@ -54,22 +83,18 @@ class App extends Component {
 
     let persons = null;
 
-    if ( this.state.showPersons  ) {
+    if ( this.state.showPersons ) {
       persons = (
         <div>
-         <Person
-           name={this.state.persons[0].name}
-           age={this.state.persons[0].age}
-          />
-         <Person
-           name={this.state.persons[1].name}
-           age={this.state.persons[1].age}
-           click={this.switchNameHandler.bind(this, 'Pablos')}
-           changed={this.nameChangedHandler}>My Profession: Psychiatrist</Person>
-         <Person
-           name={this.state.persons[2].name}
-           age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person, index) => {
+          // anonymous function is executed in every element in persons array
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
       </div>
       );
     }
