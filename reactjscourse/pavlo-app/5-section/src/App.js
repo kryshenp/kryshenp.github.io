dc911eaf0b38331506401f0c0624d0e6a8831cff
Pavlo Kryshenyk <pavlokryshenyk@Pavlos-MacBook-Pro.local>
2018-08-08 22:30:16 +0200

    5-66-Enabling & Using CSS Modules

--- reactjscourse/pavlo-app/5-section/src/App.js
+++ reactjscourse/pavlo-app/5-section/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 
 import Person from './Person/Person';
 
@@ -108,18 +108,18 @@ class App extends Component {
     // let classes = ['red', 'bold'].join(' ');
     // ------
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes =['red']
+      assignedClasses.push( classes.red ); // classes =['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes =['red', 'bold']
+      assignedClasses.push( classes.bold ); // classes =['red', 'bold']
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button
              style={style}
              onClick={this.togglePersonsHandler}>Toggle persons</button>
