54d44ec5711465fdc81f978a2173e4ee7cd996dd
kryshenp <kryshenykpavlo@gmail.com>
2018-10-13 14:12:22 +0200

    Performance Gains with PureComponents 7-90

--- reactjscourse/pavlo-app/7-section/src/components/Persons/Persons.js
+++ reactjscourse/pavlo-app/7-section/src/components/Persons/Persons.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
   constructor(props) {
      super(props);
      console.log( '[Persons.js] Inside Constructor', props );
@@ -21,11 +21,14 @@ class Persons extends Component {
     console.log( '[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps );
   }
 
-  // returns true or false
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log( '[UPDATE Presons.js] Inside shouldComponentUpdate', nextProps, nextState );
-    return nextProps.persons !== this.props.persons;
-  }
+  // // returns true or false
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log( '[UPDATE Presons.js] Inside shouldComponentUpdate', nextProps, nextState );
+  //   return nextProps.persons !== this.props.persons ||
+  //   nextProps.changed !== this.props.changed ||
+  //   nextProps.clicked !== this.props.clicked;
+  //   // return true;
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log( '[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState );
