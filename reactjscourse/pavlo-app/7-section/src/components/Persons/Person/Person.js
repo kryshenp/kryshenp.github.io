a531d830366c0df202ed91460bcfd701c34bc560
kryshenp <kryshenykpavlo@gmail.com>
2018-10-13 15:34:43 +0200

    Understanding Higher Order Components (HOCs) 7-96

--- reactjscourse/pavlo-app/7-section/src/components/Persons/Person/Person.js
+++ reactjscourse/pavlo-app/7-section/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import WithClass from '../../../hoc/WithClass';
 
 import classes from './Person.css';
 
@@ -20,11 +21,11 @@ class Person extends Component {
   render () {
     console.log('[Person.js] Inside render()')
     return (
-      <div className={classes.Person}>
+      <WithClass classes={classes.Person}>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </div>
+      </WithClass>
     )
 
     // return [
--- reactjscourse/pavlo-app/7-section/src/containers/App.js
+++ reactjscourse/pavlo-app/7-section/src/containers/App.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 
 class App extends PureComponent {
@@ -153,16 +154,18 @@ class App extends PureComponent {
     // ------
 
 
-    return (<div className={classes.App}>
-      <button onClick={() => {this.setState({showPersons: true})}} >Show Persons</button>
-      <Cockpit
-        appTitle={this.props.title}
-        showPersons={this.state.showPersons}
-        persons={this.state.persons}
-        clicked={this.togglePersonsHandler}/>
-      {persons}
-    </div>);
-    //  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+    return (
+      <WithClass classes={classes.App}>
+        <button onClick={() => {this.setState({showPersons: true})}} >Show Persons</button>
+        <Cockpit
+          appTitle={this.props.title}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler} />
+        {persons}
+    </WithClass>
+    );
+    //  return React.createElement('div', {className: App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
