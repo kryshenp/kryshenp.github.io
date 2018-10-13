b0aed76d18ec4e549e06f6831b1ac70f22164cde
kryshenp <kryshenykpavlo@gmail.com>
2018-10-13 15:48:08 +0200

    A Different Approach to HOCs 7-97

--- reactjscourse/pavlo-app/7-section/src/components/Persons/Person/Person.js
+++ reactjscourse/pavlo-app/7-section/src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import WithClass from '../../../hoc/WithClass';
+import withClass from '../../../hoc/withClass';
+import Aux from '../../../hoc/Aux';
 
 import classes from './Person.css';
 
@@ -21,11 +22,11 @@ class Person extends Component {
   render () {
     console.log('[Person.js] Inside render()')
     return (
-      <WithClass classes={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </WithClass>
+      </Aux>
     )
 
     // return [
@@ -36,4 +37,4 @@ class Person extends Component {
   }
   }
 
-export default Person;
+export default withClass(Person, classes.Person);
--- reactjscourse/pavlo-app/7-section/src/containers/App.js
+++ reactjscourse/pavlo-app/7-section/src/containers/App.js
@@ -3,7 +3,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
 
 class App extends PureComponent {
@@ -155,7 +156,7 @@ class App extends PureComponent {
 
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => {this.setState({showPersons: true})}} >Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -163,10 +164,10 @@ class App extends PureComponent {
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
         {persons}
-    </WithClass>
+      </Aux>
     );
     //  return React.createElement('div', {className: App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
