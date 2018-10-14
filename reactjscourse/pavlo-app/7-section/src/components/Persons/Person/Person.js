ce2701cb89116585b4903c2fab9acfcf400c77d8
kryshenp <kryshenykpavlo@gmail.com>
2018-10-14 11:56:57 +0200

    Validating Props 7-100

--- reactjscourse/pavlo-app/7-section/src/components/Persons/Person/Person.js
+++ reactjscourse/pavlo-app/7-section/src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import withClass from '../../../hoc/withClass';
 import Aux from '../../../hoc/Aux';
+import PropTypes from 'prop-types';
 
 import classes from './Person.css';
 
@@ -35,6 +36,13 @@ class Person extends Component {
     //   <input key="3" type="text" onChange={this.props.changed} value={this.props.name}/>
     // ]
   }
-  }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
