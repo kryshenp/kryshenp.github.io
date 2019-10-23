c1f7cdac33593258f6fcc429762bdef102154d95
kryshenp <kryshenykpavlo@gmail.com>
2019-10-23 15:20:25 +0200

    175a first approach to work on responsive adjustments

--- reactjscourse/burger-app/src/components/Navigation/Toolbar/Toolbar.js
+++ reactjscourse/burger-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import classes from './Toolbar.css';
-import Logo from '../../Layout/Logo/Logo';
+import Logo from '../../Burger/Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
+    <Logo height="80%" />
     <nav>
       <NavigationItems />
     </nav>
