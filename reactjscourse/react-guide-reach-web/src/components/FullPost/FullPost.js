b09720fbf0a60d5292aeaefd5c89fa2425119d64
kryshenp <kryshenykpavlo@gmail.com>
2019-12-05 22:15:31 +0100

    195. Make a Post Selectable

--- reactjscourse/react-guide-reach-web/src/components/FullPost/FullPost.js
+++ reactjscourse/react-guide-reach-web/src/components/FullPost/FullPost.js
@@ -3,17 +3,18 @@ import './FullPost.css'
 
 class FullPost extends Component {
   render() {
-    let post = <p>Please select a post!</p>
-    post = (
-      <div className="FullPost">
-        <h1>Title</h1>
-        <p>Content</p>
-        <div className="Edit">
-          <button className="Delete">Delete</button>
+    let post = <p style={{ textAlign: 'center' }}>Please select a post!</p>
+    if (this.props.id) {
+      post = (
+        <div className="FullPost">
+          <h1>Title</h1>
+          <p>Content</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
         </div>
-      </div>
-    )
-
+      )
+    }
     return post
   }
 }
