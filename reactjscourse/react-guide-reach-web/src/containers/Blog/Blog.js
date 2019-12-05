1ffff5c055eff0c6ddac952fa1630ba1fd774b60
kryshenp <kryshenykpavlo@gmail.com>
2019-12-05 21:45:38 +0100

    193. Render Fetched Data to the Screen

--- reactjscourse/react-guide-reach-web/src/containers/Blog/Blog.js
+++ reactjscourse/react-guide-reach-web/src/containers/Blog/Blog.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 import FullPost from '../../components/FullPost/FullPost'
 import NewPost from '../../components/NewPost/NewPost'
@@ -5,14 +6,23 @@ import Post from '../../components/Post/Post'
 import './Blog.css'
 
 class Blog extends Component {
+  state = {
+    posts: [],
+  }
+
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/posts').then(response => {
+      this.setState({ posts: response.data })
+      // console.log('response', response)
+    })
+  }
   render() {
+    const posts = this.state.posts.map(post => {
+      return <Post key={post.id} title={post.title} />
+    })
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
